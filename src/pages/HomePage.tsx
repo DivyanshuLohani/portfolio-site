f1d8bfe3368b63a203b715767c92f747f62c9b81
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2024-09-05 00:49:44 +0530

    add view all button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,21 @@
 import Home from "../components/Home";
 import Projects from "../components/Projects";
 import Timeline from "../components/Experiences";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
     <>
       <Home />
       <Projects n={4} />
+      <div className="flex justify-center mt-10">
+        <Link
+          to={"/projects"}
+          className="px-5 py-2 border rounded-full hover:text-black hover:bg-white duration-300 transition-colors"
+        >
+          View All
+        </Link>
+      </div>
       <Timeline />
     </>
   );
