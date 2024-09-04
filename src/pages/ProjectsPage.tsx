39adc60df6ee460240ca26118862eeaae032579a
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2024-09-05 01:01:59 +0530

    add scroll to top

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,6 +1,13 @@
+import { useLocation } from "react-router-dom";
 import Projects from "../components/Projects";
+import { useEffect } from "react";
 
 export default function ProjectsPage() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return (
     <>
       <Projects />
