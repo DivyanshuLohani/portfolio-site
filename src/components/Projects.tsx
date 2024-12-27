bdf9cb7ea944907e5125c30dd55e1448e59397c6
DivyanshuLohani <59534359+DivyanshuLohani@users.noreply.github.com>
2024-12-27 23:49:45 +0530

    Remove flappy bird from projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -193,19 +193,7 @@ const projects = [
       "devicon-react-plain",
       "devicon-materialui-plain",
     ],
-  },
-  {
-    // imageUrl: "",
-    url: "https://github.com/DivyanshuLohani/flappy-bird",
-    year: new Date("2023-03-01"),
-    name: "Flappy Bird",
-    description: "A Flappy Bird clone built using Raylib",
-    icons: [
-      "devicon-cplusplus-plain",
-      "devicon-raylib-plain",
-      "devicon-android-plain",
-    ],
-  },
+  }
 ];
 
 function getTechStacks() {
