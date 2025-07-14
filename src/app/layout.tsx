71f426f4c47ada7b6424832bab6d5d2b06916ff0
Divyanshu Lohani <DivyanshuLohani@users.noreply.github.com>
2025-07-14 08:52:31 +0530

    Replace N8NChat component with ChatAssistant in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import { N8NChat } from "@/components/N8NChat";
+import ChatAssistant from "@/components/ChatBot";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -38,8 +38,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        {/* <ChatAssistant /> */}
-        <N8NChat webhookUrl={process.env.N8N_URL as string} />
+        <ChatAssistant />
+        {/* <N8NChat webhookUrl={process.env.N8N_URL as string} /> */}
         <Analytics />
       </body>
     </html>
